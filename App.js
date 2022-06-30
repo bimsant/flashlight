89dd94d61113825ddcb48acaf5056210e2d6b2bb
bimsant <santos.bims@gmail.com>
2022-06-29 23:54:58 -0300

    inserindo projeto lanterna

--- App.js
+++ App.js
@@ -1,20 +1,66 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import Torch from 'react-native-torch';
+import RNShake from 'react-native-shake';
+import {View, StyleSheet, Image, TouchableOpacity, Alert, } from 'react-native';
 
-export default function App() {
+const App = () => {
+  const [TOGGLE, setToggle] = useState(false);
+
+  const HANDLE_CHANGE_TOGGLE = () => setToggle(oldToggle => !oldToggle);
+
+  useEffect(() => {
+    Torch.switchState(TOGGLE);
+  }, [TOGGLE]);
+
+  useEffect(() => {
+    const SUBSCRIBE = RNShake.addListener(() => {
+      setToggle(oldToggle => !oldToggle);
+    });
+
+    return () => SUBSCRIBE.remove();
+  });
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={TOGGLE ? style.containerLight: style.container}>
+      <TouchableOpacity onPress={HANDLE_CHANGE_TOGGLE}>
+        <Image 
+          style={TOGGLE ? style.lightingOn : style.lightingOff}
+          source={
+            TOGGLE
+            ? require('./assets/icons/eco-light.png')
+            : require('./assets/icons/eco-light-off.png')} />
+      </TouchableOpacity>
     </View>
-  );
-}
+  )
+};
 
-const styles = StyleSheet.create({
+
+export default App;
+
+const style = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  containerLight: {
+    flex: 1,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  lightingOn: {
+    resizeMode: 'contain',
+    alignSelf: 'center',
+    width: 150,
+    height: 150,
+  },
+  lightingOff: {
+    resizeMode: 'contain',
+    alignSelf: 'center',
+    tintColor: 'white',
+    width: 150,
+    height: 150,
   },
 });
---
+++ README.md
@@ -0,0 +1,12 @@
+# Projeto Lanterna
+
+Olá! 
+
+Este repositório foi feito com um projeto realizado a partir do Bootcamp da Decola Tech, 
+mentorado pelo Ismael Moreira.
+
+Ele contém o projeto Flashlight, onde eu codifiquei uma lanterna utilizando o React Native.
+
+Fica a vontade para dar uma olhada no meu código :happy:
+
+Até logo!
